cd744623d287e249c03051d321e09d4981f91919
Arman Feili <armanfc.enter@yahoo.com>
2018-09-21 18:04:19 +0430

    added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('unable to connect to server', err);
+  }
+  console.log('connected to server');
+
+  const db = client.db('TodoApp');
+
+  //   db.collection('Todos').findOneAndUpdate(
+  //     {
+  //       _id: new ObjectID('5ba4ecd147ed5ef52a034fe5')
+  //     },
+  //     {
+  //       $set: {
+  //         completed: true
+  //       }
+  //     }, {
+  //       returnOriginal: false
+  //     }).then((result) => {
+  //     console.log(result)
+  //   })
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ba4f15f47ed5ef52a0351f4')
+  },
+    {
+      $set: {
+        name: 'Ariana'
+      },
+      $inc: {
+        age: -2
+      }
+    },
+    {
+      returnOriginal: false
+    });
+
+  client.close();
+});
