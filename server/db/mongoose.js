29d2253330ae075202ecf37abf62206c1a848347
Arman Feili <armanfc.enter@yahoo.com>
2018-09-28 11:35:26 +0330

    seprate test database from localhost and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
-mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || 'mongodb://127.0.0.1:27017:27017/TodoApp', { useNewUrlParser: true });
+// mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || 'mongodb://127.0.0.1:27017:27017/TodoApp', { useNewUrlParser: true })
+
+process.env.MONGODB_URI = 'mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab';
+const localhost = '127.0.0.1:27017';
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true });
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 // mongoose.connect(
 //   'mongodb://localhost:27017/TodoApp',
