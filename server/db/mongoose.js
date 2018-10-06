93a759ae1faa793dfbe879c6a53f7225576e7d8d
Arman Feili <armanfc.enter@yahoo.com>
2018-10-06 17:17:20 +0330

    updating config to use JSON file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,10 +4,13 @@
 // // mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 // // mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || 'mongodb://127.0.0.1:27017:27017/TodoApp', { useNewUrlParser: true })
 
-// process.env.MONGODB_URI = 'mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab'
+// // process.env.MONGODB_URI = 'mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab'
 // const localhost = '127.0.0.1:27017'
 
-// mongoose.connect(process.env.MONGODB_URI || `mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true })
+// mongoose.connect(`mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true })
+
+// // console.log(process.env.MONGODB_URI); // mongodb://localhost:27017/TodoApp
+
 // // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 // // mongoose.connect(
 // //   'mongodb://localhost:27017/TodoApp',
@@ -22,7 +25,9 @@ const localhost = '127.0.0.1:27017';
 // process.env.MONGODB_URI = 'mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true })
+mongoose.connect('mongodb://armandomlab:arbamlab756@ds113693.mlab.com:13693/herokumlab' || `mongodb://${localhost}:27017/TodoApp`, { useNewUrlParser: true });
+
 console.log(process.env.MONGODB_URI); // mongodb://localhost:27017/TodoApp
 
 module.exports = { mongoose};
